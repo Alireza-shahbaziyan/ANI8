8ff61a9ae047c9884cc5ab36ede76753a6aea4b0
Alireza shahbaziyan <87496598+Alireza-shahbaziyan@users.noreply.github.com>
2023-02-06 13:41:22 -0800

    change navbar bahman/17

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,14 @@
 <template>
-    <header class="bg-cover bg-center bg- w-full h-screen">
-    <Navbar/>
-        <div class="text-white md:text-xl font-bold md:w-1/3 w-10/12 h-2/3 flex flex-col items-center justify-center">
+    <header class="bg-cover bg-center bg- w-full h-screen ">
+    <ResponsiveNavbar/>
+        <div class="text-white md:text-xl font-bold md:w-1/3 w-10/12 h-2/3 flex flex-col items-center justify-center mx-8">
             <div class="md:text-9xl text-6xl font-black font-sans w-full uppercase">Ani8</div>
             If you like anime or manga, Ani8 is made for you.
         </div>
     </header>
 </template>
 <script>
-import Navbar from './Navbar.vue';
+import ResponsiveNavbar from './ResponsiveNavbar.vue';
 
 export default{
     name: "Header",
@@ -17,7 +17,7 @@ export default{
             aniName: "demon slayer",
         };
     },
-    components: { Navbar }
+    components: {ResponsiveNavbar }
 }
 </script>
 <style scoped>
