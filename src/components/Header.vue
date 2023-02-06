dea76fa6c3f32d0603755b8019a1f387bb4b47d2
Alireza shahbaziyan <87496598+Alireza-shahbaziyan@users.noreply.github.com>
2023-02-05 17:42:05 -0800

    frist commit

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <header class="bg-cover bg-center bg- w-full h-screen">
+    <Navbar/>
+        <div class="text-white text-xl font-bold w-1/3 h-2/3 flex flex-col items-center justify-center">
+            <div class="text-9xl font-black font-sans w-full uppercase">Ani8</div>
+            If you like anime or manga, Ani8 is made for you.
+        </div>
+    </header>
+</template>
+<script>
+import Navbar from './Navbar.vue';
+
+export default{
+    name: "Header",
+    data() {
+        return {
+            aniName: "demon slayer",
+        };
+    },
+    components: { Navbar }
+}
+</script>
+<style scoped>
+header{
+    background-image: url('../assets/images/image11.jpg');
+}
+</style>
